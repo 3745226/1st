ee7467d78d0bb30f462dadc0e92fe5d22f201f2c
3745226 <bezczeszczenieCietrzewia#2021>
2021-02-22 23:54:36 +0100

    FirstTimeMayHurt

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Just joking! Will be back in 30 days!!! from tomorrow ofcbro (-:");
+        int i=30;
+        System.out.println("It looks like there are(or is?) still "+i+" days missing");
+    }
+}
